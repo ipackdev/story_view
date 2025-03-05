a21711681b40bfa325d5e789dfd9b21ee1a3217e
pack_dev <i.pack.dev@gmail.com>
2025-03-05 10:15:12 +0300

    upd: `StoryVideo`

--- lib/widgets/story_video.dart
+++ lib/widgets/story_video.dart
@@ -1,50 +1,12 @@
 import 'dart:async';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_cache_manager/flutter_cache_manager.dart';
-
-import '../controller/story_controller.dart';
-import '../utils.dart';
-
-class VideoLoader {
-  String url;
-
-  File? videoFile;
-
-  Map<String, dynamic>? requestHeaders;
-
-  LoadState state = LoadState.loading;
-
-  VideoLoader(this.url, {this.requestHeaders});
-
-  void loadVideo(VoidCallback onComplete) {
-    if (videoFile != null) {
-      state = LoadState.success;
-      onComplete();
-    }
-
-    final fileStream = DefaultCacheManager()
-        .getFileStream(url, headers: requestHeaders as Map<String, String>?);
-
-    fileStream.listen((fileResponse) {
-      if (fileResponse is FileInfo) {
-        if (videoFile == null) {
-          state = LoadState.success;
-          videoFile = fileResponse.file;
-          onComplete();
-        }
-      }
-    });
-  }
-}
+import 'package:story_view/controller/story_controller.dart';
 
 class StoryVideo extends StatefulWidget {
-  StoryVideo(
-    this.videoLoader, {
-    Key? key,
+  const StoryVideo({
+    super.key,
     required this.storyController,
-    // playerController!.value.aspectRatio
     required this.playerAspectRatio,
     required this.playerCRInit,
     required this.onPause,
@@ -52,7 +14,7 @@ class StoryVideo extends StatefulWidget {
     required this.videoWidget,
     this.loadingWidget,
     this.errorWidget,
-  }) : super(key: key ?? UniqueKey());
+  });
 
   final StoryController storyController;
 
@@ -62,8 +24,6 @@ class StoryVideo extends StatefulWidget {
   final double playerAspectRatio;
   final Widget? videoWidget;
 
-  final VideoLoader videoLoader;
-
   final Widget? loadingWidget;
   final Widget? errorWidget;
 
@@ -72,10 +32,8 @@ class StoryVideo extends StatefulWidget {
 }
 
 class StoryVideoState extends State<StoryVideo> {
-  Future<void>? playerLoader;
-
   StreamSubscription? _streamSubscription;
-  bool videoPRisInitialized = false;
+  bool _videoPRisInitialized = false;
 
   @override
   void initState() {
@@ -83,64 +41,37 @@ class StoryVideoState extends State<StoryVideo> {
 
     widget.storyController.pause();
 
-    widget.videoLoader.loadVideo(() {
-      if (widget.videoLoader.state == LoadState.success) {
-        widget.playerCRInit.call().then((_) {
-          setState(() {
-            videoPRisInitialized = true;
-          });
-          widget.storyController.play();
+    widget.playerCRInit.call().then((_) {
+      if (mounted) {
+        setState(() {
+          _videoPRisInitialized = true;
         });
+      }
+    });
 
-        _streamSubscription =
-            widget.storyController.playbackNotifier.listen((playbackState) {
-          if (playbackState == PlaybackState.pause) {
-            widget.onPause();
-          } else {
-            widget.onPlay();
-          }
-        });
-      } else {
-        setState(() {});
+    _streamSubscription =
+        widget.storyController.playbackNotifier.listen((playbackState) {
+      if (playbackState == PlaybackState.pause) {
+        widget.onPause();
+      } else if (playbackState == PlaybackState.play) {
+        widget.onPlay();
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return ColoredBox(
       color: Colors.black,
-      height: double.infinity,
-      width: double.infinity,
-      child: Builder(
-        builder: (context) {
-          if (widget.videoLoader.state == LoadState.success &&
-              videoPRisInitialized) {
-            return Center(
-              child: AspectRatio(
-                aspectRatio: widget.playerAspectRatio,
-                child: widget.videoWidget,
-              ),
-            );
-          }
-
-          return Center(
-            child: widget.videoLoader.state == LoadState.loading
-                ? widget.loadingWidget ??
-                    const SizedBox.square(
-                      dimension: 70,
-                      child: CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                        strokeWidth: 3,
-                      ),
-                    )
-                : widget.errorWidget ??
-                    const Text(
-                      "Media failed to load",
-                      style: TextStyle(color: Colors.white),
-                    ),
-          );
-        },
+      child: SizedBox.expand(
+        child: Center(
+          child: _videoPRisInitialized
+              ? AspectRatio(
+                  aspectRatio: widget.playerAspectRatio,
+                  child: widget.videoWidget,
+                )
+              : widget.loadingWidget ?? const CircularProgressIndicator(),
+        ),
       ),
     );
   }
