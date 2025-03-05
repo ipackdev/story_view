2443471277b5ebab5cac78666d4a70bcef5d2d97
pack_dev <i.pack.dev@gmail.com>
2025-03-05 19:03:43 +0300

    del: story image

--- lib/story_view.dart
+++ lib/story_view.dart
@@ -3,6 +3,5 @@ library;
 
 export 'controller/story_controller.dart';
 export 'utils.dart';
-export 'widgets/story_image.dart';
 export 'widgets/story_video.dart';
 export 'widgets/story_view.dart';
